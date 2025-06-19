63322a66f71dcb76d4ae8c9f96bd4943f569ee44
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-19 21:29:12 +0100

    Template Striing done

---
+++ templateString.js
@@ -0,0 +1,8 @@
+let myString = `Atikur Rahaman Shuvo`;
+console.log(myString);
+
+let firstName = "Atikur";
+let lastName = "Shuvo";
+
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
