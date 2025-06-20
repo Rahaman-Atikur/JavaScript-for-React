7e5f03c1aa02a7b839d474758a79be09d97ea1eb
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-20 08:57:55 +0100

    Destructuring not done

---
+++ Destructing.js
@@ -0,0 +1,13 @@
+// Array Destructuring
+let personName = ["Atik", "Shuvo", "Trisha"];
+let [ok,nice] = personName;
+console.log(ok);
+
+// Object Destructuring
+let humanBeing = 
+    {name:"Atik",
+        age:28
+    };
+
+let {myname,myage}=humanBeing;
+console.log(myname);
