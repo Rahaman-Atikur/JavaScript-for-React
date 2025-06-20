83df2a295c4dd0e887dd2284b2b990d9aa9bdc29
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-20 13:42:19 +0100

    Local storage done

--- Destructing.js
+++ Destructing.js
@@ -1,13 +1,25 @@
 // Array Destructuring
 let personName = ["Atik", "Shuvo", "Trisha"];
 let [ok,nice] = personName;
-console.log(ok);
+// console.log(ok);
 
 // Object Destructuring
-let humanBeing = 
-    {name:"Atik",
-        age:28
-    };
+let humanBeing = {
+    name: "Atik",
+    sex:"Male",
+    age:43
+};
+let {name:newName}=humanBeing;
+console.log(newName);
 
-let {myname,myage}=humanBeing;
-console.log(myname);
+let objectNotation = JSON.stringify(humanBeing)
+console.log(objectNotation);
+
+let reverseObject = JSON.parse(objectNotation);
+console.log(reverseObject);
+
+// truthy and falsy value 
+
+
+// Local Storage
+   
