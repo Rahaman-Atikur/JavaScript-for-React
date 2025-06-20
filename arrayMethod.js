490ea7492cae47fd801f17aa63b83e57dbe22833
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-20 08:14:24 +0100

    find method not done

--- arrayMethod.js
+++ arrayMethod.js
@@ -1,8 +1,24 @@
-const myArray = [10,20,30,40,50];
+// Map Method
+const myArray = [10, 20, 30, 40, 50];
 
-
-const myFun=(numbers)=>{
-    return numbers*10;
-}
-let newArray =myArray.map(myFun);
+const myFun = (numbers) => {
+  return numbers * 10;
+};
+let newArray = myArray.map(myFun);
 console.log(newArray);
+// forEach Method
+const fruitsList = ["Apple", "Banana", "Grapes", "Strawberry"];
+const showFruits = (fruits) => {
+  console.log(fruits);
+};
+fruitsList.forEach(showFruits);
+// Filter Method
+let ageOfPerson = [22, 18, 19, 45,12];
+let checkAdult = (age) => {
+  return age >= 18;
+};
+let adult=ageOfPerson.filter(checkAdult);
+console.log(adult);
+
+// Find Method
+
